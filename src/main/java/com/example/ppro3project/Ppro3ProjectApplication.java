a9b3685ba67dcb6ad19c8cd448cb66a9f791e1ba
Marek <ruzicmarek@centrum.cz>
2024-10-02 11:01:19 +0200

    Start

---
+++ src/main/java/com/example/ppro3project/Ppro3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ppro3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppro3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ppro3ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ppro3project/Ppro3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppro3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppro3ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
